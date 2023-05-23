c55be3803d907afca1914aa30b1c85f0e19164cc
jaiky kumar <40190790+jaikysh76@users.noreply.github.com>
2023-05-23 18:40:49 +0530

    Add files via upload

---
+++ program1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "now we are new in shell scripting"
+
+read color
+
+if [ $color== red ] #$  output checkin linux
+then
+     echo "Devops"
+
+elif [$color == blue]
+then
+	echo "cloud computing"
+
+else
+	echo"FSD"
+
+fi
+
